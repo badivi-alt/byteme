b55f71bf9012ca7ac3ab706320c36efd3a2537fc
Ben Adivi <benadivi@Mac.home>
2025-08-21 15:48:06 -1000

    fix: restore proper 'use client' directive

--- src/app/(pages)/(dashboard)/page.tsx
+++ src/app/(pages)/(dashboard)/page.tsx
@@ -1,4 +1,4 @@
-&apos;use client&apos;;
+'use client';
 
 import React, { useState, useEffect } from "react";
 import { User, PlanItem, Task, Event, Curriculum, Topic } from "@/entities/all";
--- src/app/(pages)/(dashboard)/today/page.tsx
+++ src/app/(pages)/(dashboard)/today/page.tsx
@@ -1,4 +1,4 @@
-&apos;use client&apos;;
+'use client';
 
 import React, { useState, useEffect } from "react";
 import { User, PlanItem, Task } from "@/entities/all";
